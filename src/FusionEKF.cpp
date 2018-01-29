23136a69b2ffdf78d0b4ec456a9ddadcb0bbf53b
yiding <liangyd@umich.edu>
2018-01-28 21:08:55 -0500

    complete kalman filter and fusion

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -36,6 +36,14 @@ FusionEKF::FusionEKF() {
     * Finish initializing the FusionEKF.
     * Set the process and measurement noises
   */
+  ekf_.P_=MatrixXd(4,4);
+  ekf_.P_ << 1, 0, 0, 0,
+             0, 1, 0, 0,
+             0, 0, 1000, 0,
+	     0, 0, 0, 1000;
+
+  H_laser_<<1, 0, 0, 0,
+	    0, 1, 0, 0;
 
 
 }
@@ -58,6 +66,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       * Create the covariance matrix.
       * Remember: you'll need to convert radar from polar to cartesian coordinates.
     */
+	
     // first measurement
     cout << "EKF: " << endl;
     ekf_.x_ = VectorXd(4);
@@ -67,12 +76,23 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
       /**
       Convert radar from polar to cartesian coordinates and initialize state.
       */
+	
+	float ro=measurement_pack.raw_measurements_[0];
+	float theta=measurement_pack.raw_measurements_[1];
+	ekf_.x_[0]=ro*cos(theta);
+	ekf_.x_[1]=ro*sin(theta);
+	
+
     }
     else if (measurement_pack.sensor_type_ == MeasurementPackage::LASER) {
       /**
       Initialize state.
-      */
+      */	
+	ekf_.x_[0]=measurement_pack.raw_measurements_[0];
+	ekf_.x_[1]=measurement_pack.raw_measurements_[1];
     }
+    // save timestamp
+    previous_timestamp_=measurement_pack.timestamp_;
 
     // done initializing, no need to predict or update
     is_initialized_ = true;
@@ -90,7 +110,18 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
      * Update the process noise covariance matrix.
      * Use noise_ax = 9 and noise_ay = 9 for your Q matrix.
    */
-
+  float dt = (measurement_pack.timestamp_ - previous_timestamp_) / 1000000.0;	//dt - expressed in seconds
+  previous_timestamp_ = measurement_pack.timestamp_;
+  ekf_.F_<< 1, 0, dt, 0,
+	    0, 1, 0, dt,
+	    0, 0, 1, 0,
+	    0, 0, 0, 1;
+  float noise_ax=9;
+  float noise_ay=9;
+  ekf_.Q_<< noise_ax*pow(dt,4)/4,   0,                      noise_ax*pow(dt,3)/2,   0,
+	    0,                      noise_ay*pow(dt,4)/4,   0,                      noise_ay*pow(dt,3)/2,
+	    noise_ax*pow(dt,3)/2,   0,                      noise_ax*pow(dt,2),     0,
+	    0,                      noise_ay*pow(dt,3)/2,   0,                      noise_ay*pow(dt,2); 
   ekf_.Predict();
 
   /*****************************************************************************
@@ -105,8 +136,14 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
 
   if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
     // Radar updates
+    ekf_.H_=tools.CalculateJacobian(ekf_.x_);
+    ekf_.R_=R_radar_;
+    ekf_.UpdateEKF(measurement_pack.raw_measurements_);
   } else {
     // Laser updates
+    ekf_.H_=H_laser_;
+    ekf_.R_=R_laser_;
+    ekf_.Update(measurement_pack.raw_measurements_);
   }
 
   // print the output
--- src/tools.cpp
+++ src/tools.cpp
@@ -12,14 +12,65 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
     * Calculate the RMSE here.
   */
+  VectorXd rmse(4);
+  rmse << 0,0,0,0;
+
+  if(estimations.size()==0||estimations.size()!=ground_truth.size()){
+        cout<<"CalculateRMSE()-invalid input"<<endl;
+        return rmse;
+  }
+  //accumulate squared residuals
+  VectorXd sub(4);
+  sub << 0,0,0,0;
+  VectorXd sum(4);
+  sum << 0,0,0,0;
+  for(int i=0; i < estimations.size(); ++i){
+	sub=estimations[i]-ground_truth[i];
+	sum=sum.array()+sub.array()*sub.array();
+  }
+
+  //calculate the mean
+  VectorXd mean(4);
+  mean << 0,0,0,0;
+  mean=sum/estimations.size();
+  //calculate the squared root
+  rmse=mean.array().sqrt();
+  //return the result
+  return rmse;  
+
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
   /**
-  TODO:
     * Calculate a Jacobian here.
   */
+  MatrixXd Hj(3,4);
+
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  //check division by zero
+  if(px==0&&py==0){
+    cout<<"CalculateJacobian()-Error-Division by zero"<<endl;
+    Hj<<0,0,0,0,
+        0,0,0,0,
+        0,0,0,0;
+  }
+  else{
+    double den=sqrt(px*px+py*py);
+    Hj(0,0)=px/den;
+    Hj(0,1)=py/den;
+    Hj(1,0)=-py/pow(den,2);
+    Hj(1,1)=px/pow(den,2);
+    Hj(2,0)=py*(vx*py-vy*px)/pow(den,3);
+    Hj(2,1)=px*(vy*px-vx*py)/pow(den,3);
+    Hj(2,2)=px/den;
+    Hj(2,3)=py/den;
+  }
+  
+  return Hj;
 }
